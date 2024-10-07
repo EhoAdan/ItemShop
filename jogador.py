835ea507759b3bde5b6a4df97cc2ea98b6c98778
GabScalon <gabrielmcsscalon@gmail.com>
2024-10-07 19:32:26 -0300

    Update jogador.py

--- jogador.py
+++ jogador.py
@@ -1,6 +1,6 @@
-class Jogador
+class Jogador:
     
-    def __init__(self, nome: str, saldo: int, dinheiro_gasto: int, amigo: Amigo)
+    def __init__(self, nome: str, saldo: int, dinheiro_gasto: int, amigo: Amigo):
         if isinstance(nome, str):
             self.__nome = nome
         if isinstance(saldo, int):
@@ -14,7 +14,7 @@ class Jogador
     def nome(self):
         return self.__nome
 
-    @origem.setter
+    @nome.setter
     def nome(self, nome):
         self.__nome = nome
     
@@ -22,7 +22,7 @@ class Jogador
     def saldo(self):
         return self.__saldo
 
-    @origem.setter
+    @saldo.setter
     def saldo(self, saldo):
         self.__saldo = saldo
     
@@ -30,7 +30,7 @@ class Jogador
     def dinheiro_gasto(self):
         return self.__dinheiro_gasto
 
-    @origem.setter
+    @dinheiro_gasto.setter
     def dinheiro_gasto(self, dinheiro_gasto):
         self.__dinheiro_gasto = dinheiro_gasto
     
@@ -38,26 +38,32 @@ class Jogador
     def amigo(self):
         return self.__amigo
 
-    @origem.setter
+    @amigo.setter
     def amigo(self, amigo):
         self.__amigo = amigo
     
-    def adicionar_amigo(self, amigo: Amigo)
+    def adicionar_amigo(self, amigo: Amigo):
         if isinstance(amigo, Amigo):
             amigo = Amigo(nome, ID(?))
             if not (any(amigo.nome == nome
                     for amigo in self.__amigos)):
                 self.__amigos.append(amigo)
 
-    def excluir_amigo(self, amigo: Amigo)
+    def excluir_amigo(self, amigo: Amigo):
         for amigo in self.__amigos:
             if amigo.nome == nome:
                 self.__amigos.remove(amigo)
                 break
     
-    def adquirir_saldo(self, saldo: int)
+    def adquirir_saldo(self):
+        saldo = input("Insira o saldo a ser adquirido: ")
+        if isinstance(saldo, int):
+            saldo = int(saldo)
+            self.saldo += saldo
+            self.dinheiro_gasto += round(saldo * 0.02725, 2)
     
     def presentear(self, inventario: itens.nome, amigo)
         #puxar inventario do amigo
-    def comprar(self, saldo, inventario: itens.nome)
+    def comprar(self, saldo, inventario: itens.nome):
+        
     
