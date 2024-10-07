0a7805e5c8f8850c2a25443711c203c84997e8f9
EhoAdan <adansamuelpruss@gmail.com>
2024-10-07 19:33:12 -0300

    Update Loja.py

--- Loja.py
+++ Loja.py
@@ -1,13 +1,23 @@
 import Personagem
+import Skin
+import Chroma
+import Jogo
+import Jogador
 
 
-class Inventario:
+class Loja:
     
-    def __init__(self, personagem: Personagem, jogador: Jogador)
-        if isinstance(jogador, Jogador):
-            self.__jogador = jogador
+    def __init__(self, personagem: Personagem, jogador: Jogador, jogo: Jogo, skin: Skins, chroma: Chroma)
         if isinstance(personagem, Personagem):
             self.__personagem = personagem
+        if isinstance(jogador, Jogador):
+            self.__jogador = jogador
+        if isinstance(jogo, Jogo):
+            self.__jogo = jogo
+        if isinstance(skin, Skin):
+            self.__skin = skin
+        if isinstance(chroma, Chroma):
+            self.__chroma = chroma
 
     @property
     def jogador(self):
@@ -16,3 +26,15 @@ class Inventario:
     @property
     def personagem(self):
         return self.__personagem
+
+    @property
+    def jogo(self):
+        return self.__jogo
+
+    @property
+    def skin(self):
+        return self.__skin
+
+    @property
+    def chroma(self):
+        return self.__chroma
