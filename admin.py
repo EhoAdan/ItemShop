bde0d4bd42b0fa1d02687cf803fd6fae13b568bc
EhoAdan <adansamuelpruss@gmail.com>
2024-10-28 08:35:07 -0300

    Update admin.py
    Limpeza de código. Retirei varíaveis desnecessárias e coisas duplicadas.

--- admin.py
+++ admin.py
@@ -1,17 +1,8 @@
 class Admin:
-    def __init__(self, nome, id, email, senha):
-        if (isinstance(nome, str) and isinstance(id, int) and 
+    def __init__(self, nome, email, senha):
+        if (isinstance(nome, str) and 
             isinstance(email, str) and isinstance(senha, str)):
             self.__nome = nome
-            self.__id = id
-            self.__email = email
-            self.__senha = senha
-
-    def __init__(self, nome, id, email, senha):
-        if (isinstance(nome, str) and isinstance(id, int) and 
-            isinstance(email, str) and isinstance(senha, str)):
-            self.__nome = nome
-            self.__id = id
             self.__email = email
             self.__senha = senha
     
@@ -22,15 +13,7 @@ class Admin:
     @nome.setter
     def nome(self, nome):
         self.__nome = nome
-    
-    @property
-    def id(self):
-        return self.__id
-    
-    @id.setter
-    def id(self, id):
-        self.__id = id
-    
+
     @property
     def email(self):
         return self.__email
