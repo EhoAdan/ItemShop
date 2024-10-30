7eb14144a792eb3c5dd7218f0c3444450bd9204f
GabScalon <gabrielmcsscalon@gmail.com>
2024-10-29 23:28:11 -0300

    eu não sei mas o vscode ta implicando

--- tela_conta.py
+++ tela_conta.py
@@ -15,6 +15,6 @@ Escolha sua opção:
                     """Caso o usuário faça algo além de inserir 1, 2, 3 ou 0, já joga um valueerror logo,
                     já que é o que já tem a resposta de erro"""
                     raise ValueError
-                return opcao_tela
+                return opcao_usuario
             except ValueError:
                 print("Favor inserir uma opção válida.")
