17fdabdceedf34e58eaec403acc6460f59e08207
EhoAdan <adansamuelpruss@gmail.com>
2024-11-02 16:14:54 -0300

    Update tela_jogador.py
    Preenchimento opções

--- tela_jogador.py
+++ tela_jogador.py
@@ -1,4 +1,21 @@
 class TelaJogador():
 
-    def jogador_opcao(self):
-        pass
+    def jogador_opcoes(self):
+        print("""-------- O que você quer fazer? ---------
+0- Fechar o Jogo
+1- Jogar partida
+2- Loja
+3- Amigos
+4- Excluir conta
+""")
+        return self.jogador_selecionar_opcao_int("Selecione uma opção: ", 4, 0)
+    
+    def jogador_selecionar_opcao_int(self, mensagem, limite_superior, limite_inferior):
+        while True:
+            try:
+                opcao_usuario = int(input(mensagem))
+                if not limite_inferior <= opcao_usuario <= limite_superior:
+                    raise ValueError
+                return opcao_usuario
+            except ValueError:
+                print("Favor inserir uma opção válida.")
