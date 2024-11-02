8d9e76b05fa15ea94de25f94b3e39db155d3fa09
EhoAdan <adansamuelpruss@gmail.com>
2024-11-02 16:14:33 -0300

    Update jogador.py
    Modificações dos atributos

--- jogador.py
+++ jogador.py
@@ -3,13 +3,16 @@ from personagem import Personagem
 
 class Jogador:
     
-    def __init__(self, nome: str, email: str, senha: str, saldo: int = 0, dinheiro_gasto: int = 0):
+    def __init__(self, nome: str, email: str, senha: str, saldo: int = 0, dinheiro_gasto: int = 0, presentes_dados: int = 0, presentes_recebidos: int = 0, partidas_jogadas: int = 0):
         # acho que a gente faz essa checagem de tipos na hora de criar a conta
         self.__nome = nome
         self.__email = email
         self.__senha = senha
         self.__saldo = saldo
         self.__dinheiro_gasto = dinheiro_gasto
+        self.__presentes_dados = presentes_dados
+        self.__presentes_recebidos = presentes_recebidos
+        self.__partidas_jogadas = partidas_jogadas
         self.__amigos = []
         self.__personagens = []
         self.__skins = []
@@ -23,6 +26,22 @@ class Jogador:
     @nome.setter
     def nome(self, nome):
         self.__nome = nome
+
+    @property
+    def email(self):
+        return self.__email
+
+    @email.setter
+    def email(self, email):
+        self.__email = email
+
+    @property
+    def senha(self):
+        return self.__senha
+
+    @senha.setter
+    def senha(self, senha):
+        self.__senha = senha
 
     @property
     def saldo(self):
@@ -39,6 +58,30 @@ class Jogador:
     @dinheiro_gasto.setter
     def dinheiro_gasto(self, dinheiro_gasto):
         self.__dinheiro_gasto = dinheiro_gasto
+
+    @property
+    def presentes_dados(self):
+        return self.__presentes_dados
+
+    @presentes_dados.setter
+    def presentes_dados(self, presentes_dados):
+        self.__presentes_dados = presentes_dados
+
+    @property
+    def presentes_recebidos(self):
+        return self.__presentes_recebidos
+
+    @presentes_recebidos.setter
+    def presentes_recebidos(self, presentes_recebidos):
+        self.__presentes_recebidos = presentes_recebidos
+
+    @property
+    def partidas_jogadas(self):
+        return self.__partidas_jogadas
+
+    @partidas_jogadas.setter
+    def partidas_jogadas(self, partidas_jogadas):
+        self.__partidas_jogadas = partidas_jogadas
 
     @property
     def amigos(self):
@@ -162,5 +205,3 @@ class Jogador:
     
     def excluir_compra_historico(self, entrada):
         self.__historico_compras.pop(entrada)
-
-        
